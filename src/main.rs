42f228b9f92016a0751fc49999ef8389b6d8831e
KikyoNanakusa <nanakusa.vrc@gmail.com>
2026-03-31 02:46:25 +0900

    Add simple commands

--- src/main.rs
+++ src/main.rs
@@ -8,21 +8,58 @@ mod disassemble;
 mod llir;
 mod ssa;
 
+enum Command {
+    MinimalSSA, 
+    PrunedSSA, 
+    LLIR, 
+}
+
 fn main() -> Result<()> {
-    let analysis = disassemble::load_elf_analysis(Path::new("testcases/donut.out"))?;
-    let program = llir::lower::lower_functions(&analysis.instructions, &analysis.functions);
-    // println!("{program}");
-    for func in program.functions.iter() {
-        let start = Instant::now();
-        let ssa = ssa::construct::construct_minimal_ssa(func);
-        // let ssa = ssa::construct::construct_pruned_ssa(func);
-        let elapsed = start.elapsed();
-        println!(
-            "[timing] construct_pruned_ssa({name}): {elapsed_ms:.3} ms\n\n",
-            name = func.name,
-            elapsed_ms = elapsed.as_secs_f64() * 1000.0
-        );
-        println!("=== {name} ===\n{ssa}", name = func.name);
+    let args = std::env::args().collect::<Vec<String>>();
+    let command = args.get(1).unwrap_or_else(|| {
+        eprintln!("Usage: {} <command>", args[0]);
+        eprintln!("Commands:");
+        eprintln!("  minimal-ssa");
+        eprintln!("  pruned-ssa");
+        eprintln!("  llir");
+        std::process::exit(1);
+    });
+    let binary_path = args.get(2).unwrap_or_else(|| {
+        eprintln!("Usage: {} <command> <binary path>", args[0]);
+        std::process::exit(1);
+    });
+    let command = match command.as_str() {
+        "minimal-ssa" => Command::MinimalSSA,
+        "pruned-ssa" => Command::PrunedSSA,
+        "llir" => Command::LLIR,
+        _ => {
+            eprintln!("Invalid command: {}", command);
+            std::process::exit(1);
+        }
+    };
+
+    match command {
+        Command::MinimalSSA => {
+            let analysis = disassemble::load_elf_analysis(Path::new(binary_path))?;
+            let program = llir::lower::lower_functions(&analysis.instructions, &analysis.functions);
+            for func in program.functions.iter() {
+                let ssa = ssa::construct::construct_minimal_ssa(func);
+                println!("=== {name} ===\n{ssa}", name = func.name);
+            }
+        },
+        Command::PrunedSSA => {
+            let analysis = disassemble::load_elf_analysis(Path::new(binary_path))?;
+            let program = llir::lower::lower_functions(&analysis.instructions, &analysis.functions);
+            for func in program.functions.iter() {
+                let ssa = ssa::construct::construct_pruned_ssa(func);
+                println!("{ssa}");
+            }
+        },
+        Command::LLIR => {
+            let analysis = disassemble::load_elf_analysis(Path::new(binary_path))?;
+            let program = llir::lower::lower_functions(&analysis.instructions, &analysis.functions);
+            println!("{program}");
+        }
     }
     Ok(())
 }
